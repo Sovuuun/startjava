fd33756cc44a1ca6a47e9221a0a390e38b37cda0
Sovuuun <antonmetelev93@gmail.com>
2021-08-19 22:20:17 +0300

    implement HW1_1

---
+++ Lesson1/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        byte core = 2;
+        short ramMemory = 12;
+        int memory = 1024;
+        long ssdMemory = 120;
+        float cpuFrequency = 2.0f;
+        double gpuFrequency = 1122.0;
+        char model = 1123;
+        boolean portable = true;
+
+        System.out.println(core);
+        System.out.println(ramMemory);
+        System.out.println(memory);
+        System.out.println(ssdMemory);
+        System.out.println(cpuFrequency);
+        System.out.println(gpuFrequency);
+        System.out.println(model);
+        System.out.println(portable);
+    }
+}
