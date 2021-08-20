a6850d853304a0c20ffd3d4b8eb15958726afeaa
Sovuuun <antonmetelev93@gmail.com>
2021-08-20 21:17:54 +0300

    откорректированы имена переменных

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,9 +1,9 @@
 public class Variable {
     public static void main(String[] args) {
         byte core = 2;
-        short ramMemory = 12;
-        int memory = 1024;
-        long ssdMemory = 120;
+        short ram = 12;
+        int hdd = 1024;
+        long ssd = 120;
         float cpuFrequency = 2.0f;
         double gpuFrequency = 1122.0;
         char model = 1123;
